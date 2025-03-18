92d2ae3af94e58ff9d2da72de63646b548fc7270
thomas alfroy <thomas.alfroy@etu.unistra.fr>
2025-03-18 10:37:18 +0100

    remove artifcats

--- setup.py
+++ setup.py
@@ -32,11 +32,11 @@ setup(
     name='pygillstream',
     version='0.1.2',
     packages=find_packages(),
-    install_requires=['requests>=2.24.0'],  # If you have dependencies, list them here
+    install_requires=['requests>=2.24.0'],
     author='Thomas Holterbach and Thomas Alfroy',
     author_email='contact@bgproutes.io',
     description='A Python library to get BGP data from GILL',
-    url='https://forge.icube.unistra.fr/thomas.alfroy/pygillstream',  # Update this to your repo URL
+    url='https://github.com/talfroy/pygillstream',
     classifiers=[ ],
     python_requires='>=3.6'
 )
